5ea866be24b62fdc42c90f7d75c5794ad1c46547
carolineevail <carolineevail@gmail.com>
2016-03-29 10:08:43 -0400

    Added 4 of a kind

--- src/cards_clojure/core.clj
+++ src/cards_clojure/core.clj
@@ -5,12 +5,18 @@
 (def ranks (range 1 14))
 (def rank-names {1 :ace, 11 :jack, 12 :queen, 13 :king})
 
+(def test-four-of-a-kind #{{:suit :clubs:rank 1}
+                           {:suit :spades :rank 1}
+                           {:suit :diamonds :rank 1}
+                           {:suit :hearts :rank 1}})
+
 (defn create-deck []
   (set
     (for [suit suits
           rank ranks]
       {:suit suit
-       :rank (get rank-names rank rank)})))
+       :rank rank})))
+;       :rank (get rank-names rank rank)})))
 
 (defn create-hands [deck]
   (set
@@ -22,6 +28,9 @@
 
 (defn flush? [hand]
   (= 1 (count (set (map :suit hand)))))
+
+(defn four-of-a-kind? [hand]
+  (= 1 (count (set (map :rank hand)))))
       
 
 (defn -main []
